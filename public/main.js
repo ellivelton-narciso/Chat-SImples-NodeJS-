2e68759d629f08a56d92a7f9cc463b4fdb83bea4
Ellivelton Narciso <ellivelton.souza@gmail.com>
2022-04-11 23:45:13 -0400

    Made list of users and messages being sent.

--- public/main.js
+++ public/main.js
@@ -21,6 +21,24 @@ function renderUserList() {
         ul.innerHTML += '<li>'+i+'</li>'
     });
 }
+function addMessage (type, user, msg) {
+    let ul = document.querySelector('.chatList');
+
+    switch(type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status">'+msg+'</li>'
+        break;
+        case 'msg':
+            if(username == user) {
+                ul.innerHTML += '<li class="m-txt"><span class="me">'+user+': </span>'+msg+'</li>'
+            } else { 
+                ul.innerHTML += '<li class="m-txt"><span>'+user+': </span>'+msg+'</li>'
+            }
+            
+        break;
+    }
+}
+
 
 
 //Entrando no Chat
@@ -36,11 +54,60 @@ loginInput.addEventListener('keyup', (e) =>{
     }
 });
 
+textInput.addEventListener('keyup', (e) => {
+   if(e.keyCode ===13) {
+       let txt = textInput.value.trim();
+       textInput.value = '';
+       if(txt != '') {
+           socket.emit('send-msg', txt);
+       }
+   } 
+});
+
+//Listando Usuários
 socket.on('user-ok', (list)=> {
     loginPage.style.display = 'none';
     chatPage.style.display = 'flex';
     textInput.focus();
 
+    addMessage('status', null, 'Conectado!');
+
     userList = list;
     renderUserList();
 });
+
+socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, data.joined+' joined the chat.')
+    }
+    if(data.left) {
+        addMessage('status', null, data.left+' left the chat.')
+    }
+
+    userList = data.list;
+    renderUserList();
+});
+
+socket.on('show-msg', (data) => {
+    addMessage('msg', data.username, data.message);
+});
+
+socket.on('disconnect', () => {
+    addMessage('status', null, 'Você foi desconectado!');
+    userList = [];
+    renderUserList();
+});
+socket.on('connect_error', () => {  
+    setTimeout(() => {
+        addMessage('status', null, 'Tentando reconectar...');
+    }, 2000)  
+    
+});
+
+socket.on('reconnect', () => {
+    addMessage('status', null, 'Reconectado')
+    if(username != ''){
+      socket.emit('join-request', username)
+    }
+  })
+
--- server.js
+++ server.js
@@ -13,14 +13,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 let connectedUsers = [];
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('Conexão detectada...');
 
-    socket.on('join-request', (username)=> {
+    socket.on('join-request', (username) => {
         socket.username = username;
         connectedUsers.push(username);
         console.log(connectedUsers);
 
         socket.emit('user-ok', connectedUsers);
+        socket.broadcast.emit('list-update', {
+            joined: username,
+            list: connectedUsers
+        });
+    });
+
+    socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter(u => u != socket.username);
+        console.log(connectedUsers);
+
+        socket.broadcast.emit('list-update', {
+            left: socket.username,
+            list: connectedUsers
+        });
+    });
+
+    socket.on('send-msg', (txt) => {
+        let obj = {
+            username: socket.username,
+            message: txt
+        };
+        socket.emit('show-msg', obj);
+        socket.broadcast.emit('show-msg', obj);
     });
 });
